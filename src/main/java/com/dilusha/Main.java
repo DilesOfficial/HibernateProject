0a5e5e1af1db359441d92473c031ae632c054644
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-10 10:09:36 +0530

    Changing Table and Column Names

--- src/main/java/com/dilusha/Main.java
+++ src/main/java/com/dilusha/Main.java
@@ -8,26 +8,28 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     static void main() {
 
-        Student s1 = new  Student();
-//        s1.setsName("Pasindu");
-//        s1.setRollNo(8);
-//        s1.setsAge(23);
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Dilusha");
+        a1.setTech("Java");
+
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(com.dilusha.Student.class)
+                .addAnnotatedClass(com.dilusha.Alien.class)
                 .configure()
                 .buildSessionFactory(); //cfg.buildSessionFactory();
 
         Session session = sf.openSession();
-        s1 = session.find(Student.class, 10);
+
+        session.persist(a1);
 
         Transaction transaction = session.beginTransaction();
-        session.remove(s1);
+
         transaction.commit();
 
         session.close();
         sf.close();
 
-        System.out.println(s1);
+
     }
 }
