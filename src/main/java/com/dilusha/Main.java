ebd6e8ee3cf9f1d4bb3a6e8916e49d1f7178336b
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-10 15:23:51 +0530

    Fetching

--- src/main/java/com/dilusha/Main.java
+++ src/main/java/com/dilusha/Main.java
@@ -2,12 +2,8 @@ package com.dilusha;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     static void main() {
@@ -19,23 +15,8 @@ public class Main {
 
         Session session = sf.openSession();
 
-
-        // SELECT * FROM laptop WHERE ram=32  -> SQL
-        // from laptop where ram=32   -> HQL
-
-        String brand = "Apple";
-
-        Query query = session.createQuery("select brand, model from Laptop where brand like ?1");
-        query.setParameter(1, brand);
-        List<Object[]> laptops = query.getResultList();
-
-        //Laptop l1 = session.find(Laptop.class, 904);
-
-        for (Object[] data : laptops) {
-            System.out.println((String) data[0] + " " + (String)data[1]);
-        }
-
-        System.out.println(laptops);
+        Laptop laptop = session.getReference(Laptop.class, 900);
+        //System.out.println(laptop);
 
 
         session.close();
